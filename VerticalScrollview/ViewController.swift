b7bc4bb7c392fded1244eade7c119361900350e6
Till Gartner <till.gartner@gmail.com>
2016-10-23 12:09:14 +0700

    Have printed a line when a button is clicked to see if button clicking works...

--- VerticalScrollview/ViewController.swift
+++ VerticalScrollview/ViewController.swift
@@ -20,6 +20,10 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func buttonClicked(_ sender: UIButton) {
+        let buttonName = sender.titleLabel?.text
+        print ("Button clicked: \(buttonName)")
+    }
 
 }
 
